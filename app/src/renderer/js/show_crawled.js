afbf52be5716e9cdd9b3e7fe82c5f1ddd389939c
erfan vahabi <erfanv1@gmail.com>
2020-01-20 06:33:17 +0330

    date_range get on load

--- app/src/renderer/js/show_crawled.js
+++ app/src/renderer/js/show_crawled.js
@@ -4,6 +4,16 @@ const $ = document.querySelector.bind(document)
 
 ipcRenderer.send('show_crawled_loaded')
 
+ipcRenderer.send("get_date_range")
+
+ipcRenderer.on('get_date_range_res', (e, arg) => {
+  const from = new Date(arg.from*1000)
+  $('#date_from').innerHTML = arg.from ? from.toDateString() : "_"
+  let to = new Date(arg.to*1000)
+  to.setDate(to.getDate() - 1)
+  $('#date_to').innerHTML = arg.to ? to.toDateString() : "_"
+})
+
 ipcRenderer.on('show_crawled', (e, arg) => {
   $("#result").innerHTML = ""
   arg.forEach( item => {
