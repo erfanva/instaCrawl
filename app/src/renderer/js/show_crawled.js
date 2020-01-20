3c5164437d811c5de2b51bc17e5f5174825d4764
erfan vahabi <erfanv1@gmail.com>
2020-01-20 04:08:51 +0330

    show_crawled page initialized

---
+++ app/src/renderer/js/show_crawled.js
@@ -0,0 +1,22 @@
+const { ipcRenderer } = require('electron')
+
+const $ = document.querySelector.bind(document)
+
+ipcRenderer.on('set_date_range_response', (e, arg) => {
+  $("#result").innerHTML = ""
+  arg.forEach( item => {
+    const src = item.preview_display_url
+    $("#result").innerHTML += "<img style='width: 100%' src='"+src+"'>" + "<br>"
+  })
+})
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  $('#submit').onclick = e => {
+    const from = new Date($('#date_from').value).getTime() / 1000
+    let to = new Date($('#date_to').value)
+    to.setDate(to.getDate() + 1)
+    to = to.getTime() / 1000
+
+    ipcRenderer.send("set_date_range", {from: from, to: to})
+  }
+})
